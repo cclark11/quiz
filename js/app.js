4cca83e545bee24058157d06dafc47aa7e199850
cclark11 <courtneyarc@gmail.com>
2016-10-04 23:07:23 -0400

    Second Commit

--- js/app.js
+++ js/app.js
@@ -20,6 +20,49 @@ var questions = [
 		correctAnswer:"Uneven Bars",
 		userAnswer:""
 	},	
+	{
+		question:"Who was awarded the first perfect 10 in women’s gymnastics?",
+		answers:["Shannon Miller","Nadia Comaneci","Andreea Raducan","Svetlana Khorkina"],
+		correctAnswer:"Nadia Comaneci",
+		userAnswer:""
+	},	
+	{
+		question:"On which event was the first perfect 10 received?",
+		answers:["Vault","Bars","Beam","Floor"],
+		correctAnswer:"Bars",
+		userAnswer:""
+	},	
+	{
+		question:"Who was the youngest member of the 2016 US Olympic women’s gymnastics team?",
+		answers:["Madison Kocian","Simone Biles","Gabby Douglas","Laurie Hernandez"],
+		correctAnswer:"Laurie Hernandez",
+		userAnswer:""
+	},
+	{
+		question:"The “Magnificent Seven” participated in which Olympics?",
+		answers:["1992","1996","2000","2004"],
+		correctAnswer:"1996",
+		userAnswer:""
+	},
+	{
+		question:"Which US gymnast was coached by a parent?",
+		answers:["Nastia Liukin","Carly Patterson","McKayla Maroney","Shawn Johnson"],
+		correctAnswer:"Nastia Liukin",
+		userAnswer:""
+	},
+	{
+		question:"Which US gymnast attended an Ivy League university?",
+		answers:["Amy Chow","Kirsten Maloney","Courtney McCool","Alicia Sacramone"],
+		correctAnswer:"Alicia Sacramone",
+		userAnswer:""
+	},
+	{
+		question:"Marta and Bela Karolyi’s first gym in the US was established in which city?",
+		answers:["Chappaqua, NY","Houston, TX","Burlington, ME","Los Angeles, CA"],
+		correctAnswer:"Houston, TX",
+		userAnswer:""
+	},
+
 ]
 $(document).ready(function(){
 	newGame();
@@ -37,8 +80,14 @@ $(document).ready(function(){
 			end();
 		}
 	});
-	$("#newGame").click(function(){
+	// When the user clicks on <span> (x), close the modal
+	$(".close").onclick = function() {
+	    $("#myModal").css("display","none");
+	}
+
+	$(".newGame").click(function(){
 		newGame();
+		$("#myModal").css("display","none");
 	});
 
 });
@@ -46,7 +95,7 @@ function newGame(){
 	questionNumber=0;
 	questionsCorrect=0;
 	$("#end").hide();
-	$("#count").text("");
+	$(".count").text("");
 	$("#answers").text("");
 	$("#question").text(questions[0].question);
 	for (var i=0; i<questions[0].answers.length; i++) {
@@ -57,10 +106,10 @@ function newGame(){
 function checkResponse(){
 	if(userAnswer==correctAnswer) {
 		questionsCorrect++;
-		$("#count").append("<li>Question "+(questionNumber+1)+": &#10004;</li>");
+		$(".count").append("<li>Question "+(questionNumber+1)+": &#10004;</li>");
 	}
 	else if(userAnswer!=correctAnswer) {
-		$("#count").append("<li>Question "+(questionNumber+1)+": X</li>");
+		$(".count").append("<li>Question "+(questionNumber+1)+": X</li>");
 	}
 }
 function nextQuestion(){
@@ -71,7 +120,6 @@ function nextQuestion(){
 	}
 }
 function end() {
-	$("#end").show();
-	$("#score").append("You scored "+questionsCorrect+"/"+(questions.length));
-	$("#next").hide();
+	$("#myModal").css("display","block");
+	$("#score").text("You scored "+questionsCorrect+"/"+(questions.length));
 }
